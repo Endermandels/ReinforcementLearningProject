2f1bb68b464534b929b158fb5f8e52612500c13b
Endermandels <elijahdelavar@gmail.com>
2025-03-20 11:40:29 -0700

    Create state representation

--- src/model.py
+++ src/model.py
@@ -1,18 +1,61 @@
-from time import sleep
-from controller import Controller
+import controller as ctrl
+import view as vw
+
+class Tile:
+    def __init__(self, reward: int = 0, is_obstacle: bool = False, is_terminal: bool = False):
+        self.reward = reward
+        self.is_obstacle = is_obstacle
+        self.is_terminal = is_terminal
+
+class State:
+    def __init__(self, initial_grid: list[list[Tile]]=None):
+        self.grid: list[list[Tile]] = initial_grid
+        if not self.grid:
+            self.grid = self._init_grid()
+            
+    def __repr__(self) -> str:
+        string = ""
+        for row in self.grid:
+            for tile in row:
+                if tile.is_obstacle:
+                    string += " # "  # Obstacle
+                elif tile.is_terminal:
+                    string += f" {tile.reward:+} "  # Show terminal rewards (+1, -1)
+                else:
+                    string += " . "  # Normal tile
+            string += "\n"
+        return string
+    
+    def _init_grid(self) -> list[list[Tile]]:
+        grid = [
+            [
+                Tile(), Tile(), Tile(), Tile(reward=1, is_terminal=True)
+            ],
+            [
+                Tile(), Tile(is_obstacle=True), Tile(), Tile(reward=-1, is_terminal=True)
+            ],
+            [
+                Tile(), Tile(), Tile(), Tile()
+            ]
+        ]
+        return grid
+        
 
 class Model:
     """ Keeps track of the current game state and runs the main loop """
     def __init__(self):
-        self.controller = Controller()
+        self.controller = ctrl.Controller()
+        self.view = vw.View()
+        self.cur_state = State()
 
-    def update(self):
+    def _update(self):
+        self.view.update(self.cur_state)
         self.controller.update()
 
     def run(self):
         """ Run the game loop """
         while not self.controller.should_quit():
-            self.update()
+            self._update()
 
 def main():
     model = Model()
---
+++ src/view.py
@@ -0,0 +1,43 @@
+from toolbox import cprint, bcolors
+import model as mdl
+
+class View:
+    def __init__(self, pygame_view: bool = False):
+        self.pygame_view = pygame_view # Whether to display pygame graphics or print to terminal
+    
+    def _print_grid(self, grid: list[list[mdl.Tile]]):
+        string = ""
+        for row in grid:
+            for tile in row:
+                if tile.is_terminal:
+                    string += bcolors.BOLD
+                    
+                if tile.is_obstacle:
+                    string += bcolors.YELLOW
+                    string += " # "  # Obstacle
+                elif tile.reward:
+                    if tile.reward > 0:
+                        string += bcolors.GREEN
+                    else:
+                        string += bcolors.RED
+                    string += f" {tile.reward:+} "  # Show terminal rewards (+1, -1)
+                else:
+                    string += " . "  # Normal tile
+                string += bcolors.ENDC
+            string += "\n"
+        print(string)
+        
+    def _print_state(self, state: mdl.State):
+        self._print_grid(state.grid)
+    
+    def _terminal_update(self, cur_state: mdl.State):
+        self._print_state(cur_state)
+        
+    def _pygame_update(self, cur_state: mdl.State):
+        pass
+    
+    def update(self, cur_state: mdl.State):
+        if self.pygame_view:
+            self._pygame_update(cur_state)
+        else:
+            self._terminal_update(cur_state)
