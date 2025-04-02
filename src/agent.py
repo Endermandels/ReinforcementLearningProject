6e02a1f3ba72fadc372cc50175361134e2a83836
Endermandels <elijahdelavar@gmail.com>
2025-04-02 11:02:44 -0700

    Simplify noisy actions and sensed observations

--- src/agent.py
+++ src/agent.py
@@ -1,48 +1,31 @@
 from __future__ import annotations
-from state import Action
+from state import *
 from random import choice
+from typing import NamedTuple
 
-from typing import TYPE_CHECKING
-if TYPE_CHECKING:
-    from agent_controller import AgentController
+class Observations(NamedTuple):
+    state: State
 
 class Agent:
     """ AI model that takes in observations and computes the optimal acion """
-    def __init__(self, controller: AgentController):
-        self.controller = controller
+    def __init__(self):
         self.reward = 0 # Accumulated reward over a game
 
-    def observe(self, observations: dict):
-        """ Receive observations from agent sensors """
-        # TODO: Make a sensed function wrapper
-        self._compute_action(observations)
+    def update(self, observations: Observations) -> Action:
+        """ Compute and return optimal action based on observations """
+        return Action.UP
 
-    def _compute_action(self, observations: dict):
-        """ Compute optimal action """
-        # TODO: Change to tick function
-        self._act(Action.UP)
-
-    def _act(self, action: Action):
-        """ Send action to agent controller """
-        # TODO: Make a noisy function wrapper
-        self.controller.receive_action(action)
-    
-    def receive_reward(self, reward):
-        """ Get reward based on the previous action """
-        self.reward += reward
-    
     def new_game(self):
         """ Reset agent to account for a new game """
         self.reward = 0
 
 class RandomAgent(Agent):
-    def __init__(self, controller: AgentController):
-        super().__init__(controller)
+    def __init__(self):
+        super().__init__()
 
-    def _compute_action(self, observations: dict):
-        """ Compute optimal action """
+    def update(self, observations: Observations) -> Action:
         action: Action = choice([Action.UP,
                          Action.DOWN,
                          Action.LEFT,
                          Action.RIGHT,])
-        self._act(action)
+        return action
--- src/model.py
+++ src/model.py
@@ -2,42 +2,51 @@ from __future__ import annotations
 from toolbox import warn, error
 from time import sleep
 from state import *
+from agent import *
 import controller as ctrl
 import view as vw
-import agent_controller as ag_ctrl
-import agent as ag
 import rl_agent as rl_ag
-import agent_sensors as ag_ss
 import argparse
+
+def noisy_action(action: Action) -> Action:
+    """ Returns possibly altered action """
+    return action
+
+def sensed_observations(state: State) -> Observations:
+    """ Returns possibly altered observations based on the state """
+    return Observations(state)
 
 class Model:
     """ Keeps track of the current game state and runs the main loop """
-    def __init__(self, agent: ag.Agent, agent_controller: ag_ctrl.AgentController,
-                 agent_sensors: ag_ss.AgentSensors, default_state: State,
-                 view: vw.View, controller: ctrl.Controller):
-        self.view = view
-        self.controller = controller
+    def __init__(self, 
+                 default_state: State, 
+                 agent: Agent,
+                 view: vw.View, 
+                 controller: ctrl.Controller):
         self.default_state = default_state
         self.cur_state = default_state
         self.agent = agent
-        self.agent_controller = agent_controller
-        self.agent_sensors = agent_sensors
+        self.view = view
+        self.controller = controller
 
         self.simulating_game: bool = False # Whether the agent simulation is in progress
         self.stats = Stats()
 
     def _step_agent(self):
         """ Allow agent to make an action """
-        self.agent_sensors.send_observations(self.cur_state)
+        # Perform action
+        action = noisy_action(self.agent.update(sensed_observations(self.cur_state)))
         
+        # Handle state transition
         prev_state = self.cur_state
-        self.cur_state = handle_action(self.cur_state, self.agent_controller.get_action())
+        self.cur_state = handle_action(self.cur_state, action)
+        self.stats.num_iterations += 1
         if prev_state != self.cur_state:
             self.stats.num_actions += 1
         
-        self.stats.num_iterations += 1
+        # Handle terminal state
         if self.cur_state.is_terminal:
-            self.agent_sensors.send_observations(self.cur_state)
+            self.agent.update(sensed_observations(self.cur_state))
             self.simulating_game = False
         
         self.stats.reward = self.agent.reward
@@ -123,14 +132,9 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--pygame", action="store_true")
     args = parser.parse_args()
-
-    agent_controller = ag_ctrl.AgentController()
-    agent = ag.RandomAgent(agent_controller)
-    agent_sensors = ag_ss.AgentSensors(agent)
-
+    agent = RandomAgent()
     view, controller = init_view_and_controller(args.pygame)
-
-    model = Model(agent, agent_controller, agent_sensors, TEST_STATE, view, controller)
+    model = Model(TEST_STATE, agent, view, controller)
     model.run()
 
 if __name__ == "__main__":
